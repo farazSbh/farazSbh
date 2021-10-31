96f573901367bb59acef41ca7bc75858ac35e340
farazSbh <93461883+farazSbh@users.noreply.github.com>
2021-10-31 10:14:32 +0330

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Kosta & aidanohart'
+version '1.1'
+description 'Simple & Highly customisable loading screen with a video!'
+
+loadscreen_manual_shutdown 'yes'
+loadscreen 'loading.html'
+client_script 'client/main.lua'
+
+files {
+    "css/font.css",
+    "css/plugins.css",
+    "css/style.css",
+    "js/music-controls.js",
+    "js/music-handler.js",
+    "vid/LoadingScreen.mp4",
+    "webfonts/BAUHS93.eot",
+    "webfonts/BAUHS93.ttf",
+    "webfonts/BAUHS93.woff",
+    "webfonts/HARLOWSI_1.TTF",
+    "audio.mp3",
+    "loading.html",
+    "png.png"
+}
